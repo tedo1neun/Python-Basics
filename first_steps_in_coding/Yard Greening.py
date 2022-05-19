4e76867153dcc8f79a6a2f34baa030e63ddcdd45
Teodor Todorov <104164041+tedo1neun@users.noreply.github.com>
2022-05-19 22:15:43 +0300

    Delete Yard Greening.py

--- first_steps_in_coding/Yard Greening.py
+++
@@ -1,7 +0,0 @@
-broikvmetur = float(input())
-cenakvmetri = broikvmetur * 7.61
-discount = cenakvmetri * 0.18
-discountsum = cenakvmetri - discount
-
-print(f"The final price is: {discountsum} lv.")
-print(f"The discount is: {discount} lv.")
